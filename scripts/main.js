61d78f84c4fe87b0c8eae722d6066edfadaaec37
smtjong <samanthatjong@gmail.com>
2020-05-22 18:21:11 -0400

    Improve reset function, add favicon, clean up code

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,11 @@
+/* Script for Rock, Paper, Scissors
+By Samantha Tjong */
 
-const btnTryAgain = document.getElementById('try-again');
-btnTryAgain.addEventListener('click', resetStyles);
+// Declare variables to hold the Player and Computer's scores
+let playerScore = 0;
+let computerScore = 0;
 
-function resetStyles() {
-    imgArr = document.querySelectorAll('img');
-    console.log(imgArr);
-    for (i=0; i <= imgArr.length; i++) {
-        if (imgArr[i].classList.contains('on')) {
-            imgArr[i].classList.remove('on');
-            imgArr[i].classList.add('off');
-        } continue;
-    }
-}
-
-// document.addEventListener("DOMContentLoaded", game);
-
+// Initiate a round of rock, paper, scissors when Player clicks a hand
 const btnPlayerRock = document.getElementById('playerRock');
 btnPlayerRock.addEventListener('click', playRound);
 
@@ -24,14 +15,9 @@ btnPlayerPaper.addEventListener('click', playRound);
 const btnPlayerScissors = document.getElementById('playerScissors');
 btnPlayerScissors.addEventListener('click', playRound);
 
-let playerScore = 0;
-let computerScore = 0;
-
-let playerSelection
-
-//Obtain player's input and compare to computer's hand to determine outcome of the round
+// Obtain player's input and compare to computer's hand to determine result of the round
 function playRound(btn) {
-    playerSelection = convertPlayerInput(this.id);
+    let playerSelection = convertPlayerInput(this.id);
     let computerSelection = computerPlay();
     console.log(convertPlayerInput(this.id));
     playerSelection == "ROCK" && computerSelection == "ROCK" ? case1()
@@ -46,88 +32,13 @@ function playRound(btn) {
     : case10()
 };
 
+// Convert player's input for playRound() function
 function convertPlayerInput(btnId) {
     return btnId == "playerRock" ? "ROCK"
     : btnId == "playerPaper" ? "PAPER"
     : btnId == "playerScissors" ? "SCISSORS"
     : "invalid"
 }
-
-function modifyComputerGraphic(img) {
-    img.classList.remove('off');
-    img.classList.add('on');
-};
-
-function case1() {
-    console.log("Computer played ROCK. Tie.");
-    modifyComputerGraphic(document.getElementById('computerRock').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerRock').querySelectorAll('img')[0]);
-}
-
-function case2() {
-    console.log("Computer played PAPER. You lose.")
-    modifyComputerGraphic(document.getElementById('computerPaper').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerRock').querySelectorAll('img')[0]);
-    computerScore++;
-    document.getElementById('computerScore').textContent = computerScore.toString();
-}
-
-function case3() {
-    console.log("Computer played SCISSORS. You win.")
-    modifyComputerGraphic(document.getElementById('computerScissors').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerRock').querySelectorAll('img')[0]);
-    playerScore++;
-    document.getElementById('playerScore').textContent = playerScore.toString();
-}
-
-function case4() {
-    console.log("Computer played ROCK. You win.")
-    modifyComputerGraphic(document.getElementById('computerRock').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerPaper').querySelectorAll('img')[0]);
-    playerScore++;
-    document.getElementById('playerScore').textContent = playerScore.toString();
-}
-
-function case5() {
-    console.log("Computer played PAPER. Tie.")
-    modifyComputerGraphic(document.getElementById('computerPaper').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerPaper').querySelectorAll('img')[0]);
-}
-
-function case6() {
-    console.log("Computer played SCISSORS. You lose.")
-    modifyComputerGraphic(document.getElementById('computerScissors').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerPaper').querySelectorAll('img')[0]);
-    computerScore++;
-    document.getElementById('computerScore').textContent = computerScore.toString();
-}
-
-function case7() {
-    console.log("Computer played ROCK. You lose.")
-    modifyComputerGraphic(document.getElementById('computerRock').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerScissors').querySelectorAll('img')[0]);
-    computerScore++;
-    document.getElementById('computerScore').textContent = computerScore.toString();
-}
-
-function case8() {
-    console.log("Computer played PAPER. You win.")
-    modifyComputerGraphic(document.getElementById('computerPaper').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerScissors').querySelectorAll('img')[0]);
-    playerScore++;
-    document.getElementById('playerScore').textContent = playerScore.toString();
-}
-
-function case9() {
-    console.log("Computer played SCISSORS. Tie.")
-    modifyComputerGraphic(document.getElementById('computerScissors').querySelectorAll('img')[0]);
-    modifyComputerGraphic(document.getElementById('playerScissors').querySelectorAll('img')[0]);
-}
-
-function case10() {
-    console.log("Invalid response");
-}
-
 //Generate random integer to feed into computer's hand
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -149,3 +60,117 @@ function computerPlay() {
             break;
     };
 };
+
+// Toggle "on/off" class for Player and Computer's buttons after the click
+function toggleBtnState(img) {
+    img.classList.remove('off');
+    img.classList.add('on');
+};
+
+// Detailed functions for the case of each round, feeds into playRound() function
+function case1() {
+    console.log("Computer played ROCK. Tie.");
+    toggleBtnState(document.getElementById('computerRock').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerRock').querySelectorAll('img')[0]);
+    document.getElementById('resultText').textContent = "You Tied!";
+    modal.style.display = "block";
+}
+
+function case2() {
+    console.log("Computer played PAPER. You lose.")
+    toggleBtnState(document.getElementById('computerPaper').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerRock').querySelectorAll('img')[0]);
+    computerScore++;
+    document.getElementById('computerScore').textContent = computerScore.toString();
+    document.getElementById('resultText').textContent = "You Lose!";
+    modal.style.display = "block";
+}
+
+function case3() {
+    console.log("Computer played SCISSORS. You win.")
+    toggleBtnState(document.getElementById('computerScissors').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerRock').querySelectorAll('img')[0]);
+    playerScore++;
+    document.getElementById('playerScore').textContent = playerScore.toString();
+    document.getElementById('resultText').textContent = "You Win!";
+    modal.style.display = "block";
+}
+
+function case4() {
+    console.log("Computer played ROCK. You win.")
+    toggleBtnState(document.getElementById('computerRock').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerPaper').querySelectorAll('img')[0]);
+    playerScore++;
+    document.getElementById('playerScore').textContent = playerScore.toString();
+    document.getElementById('resultText').textContent = "You Win!";
+    modal.style.display = "block";
+}
+
+function case5() {
+    console.log("Computer played PAPER. Tie.")
+    toggleBtnState(document.getElementById('computerPaper').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerPaper').querySelectorAll('img')[0]);
+    document.getElementById('resultText').textContent = "You Tied!";
+    modal.style.display = "block";
+}
+
+function case6() {
+    console.log("Computer played SCISSORS. You lose.")
+    toggleBtnState(document.getElementById('computerScissors').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerPaper').querySelectorAll('img')[0]);
+    computerScore++;
+    document.getElementById('computerScore').textContent = computerScore.toString();
+    document.getElementById('resultText').textContent = "You Lose!";
+    modal.style.display = "block";
+}
+
+function case7() {
+    console.log("Computer played ROCK. You lose.")
+    toggleBtnState(document.getElementById('computerRock').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerScissors').querySelectorAll('img')[0]);
+    computerScore++;
+    document.getElementById('computerScore').textContent = computerScore.toString();
+    document.getElementById('resultText').textContent = "You Lose!";
+    modal.style.display = "block";
+}
+
+function case8() {
+    console.log("Computer played PAPER. You win.")
+    toggleBtnState(document.getElementById('computerPaper').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerScissors').querySelectorAll('img')[0]);
+    playerScore++;
+    document.getElementById('playerScore').textContent = playerScore.toString();
+    document.getElementById('resultText').textContent = "You Win!";
+    modal.style.display = "block";
+}
+
+function case9() {
+    console.log("Computer played SCISSORS. Tie.")
+    toggleBtnState(document.getElementById('computerScissors').querySelectorAll('img')[0]);
+    toggleBtnState(document.getElementById('playerScissors').querySelectorAll('img')[0]);
+    document.getElementById('resultText').textContent = "You Tied!";
+    modal.style.display = "block";
+}
+
+function case10() {
+    console.log("Invalid response");
+}
+
+// Get the Results modal
+var modal = document.getElementById("modal");
+
+// Show results and allow player to play another round
+const btnTryAgain = document.getElementById('try-again');
+btnTryAgain.addEventListener('click', resetStyles);
+
+function resetStyles() {
+    modal.style.display = "none";
+    imgArr = document.querySelectorAll('img');
+    console.log(imgArr);
+    for (i=0; i <= imgArr.length; i++) {
+        if (imgArr[i].classList.contains('on')) {
+            imgArr[i].classList.remove('on');
+            imgArr[i].classList.add('off');
+        } continue;
+    }
+}
